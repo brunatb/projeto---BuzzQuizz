6acb35e104580d11b72c3b9cc397fc4e4383c543
brunatb <brutopanb@gmail.com>
2020-09-21 20:53:41 -0300

    Enviando usuário ao servidor

--- assets/javascript/scriptlogin.js
+++ assets/javascript/scriptlogin.js
@@ -1,8 +1,25 @@
+var identificadorUsuario;
+
 function verificarLogin(){
     var usuario = {email: "", password: ""};
     usuario.email = document.querySelector(".email").value;
     usuario.password = document.querySelector(".senha").value;
     if(usuario.email === "" || usuario.password === ""){
         alert("Preencha todos os campos!");
+    }else{
+        enviarUsuario(usuario);
     }
+}
+
+function enviarUsuario(usuario){
+    var requisicao = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v1/buzzquizz/users', usuario);
+    requisicao.then(loginSucesso).catch(loginFalha);
+}
+
+function loginSucesso(resposta){
+    identificadorUsuario = resposta.data;
+}
+
+function loginFalha(resposta){
+    console.log("erro");
 }
